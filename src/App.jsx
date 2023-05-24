a8a8322322c779b0fd6959a1e7737ec21945fe1d
Miguel Chavez <toonchavez8@gmail.com>
2023-05-23 21:30:40 -0600

    add tailwind to project

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,26 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+  return (
+    <main className="bg-slate-900 ">
+      <div className="flex justify-center bg-slate-900">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <h1 className="text-3xl font-bold underline ">Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </main>
+  );
 }
 
 export default App;
