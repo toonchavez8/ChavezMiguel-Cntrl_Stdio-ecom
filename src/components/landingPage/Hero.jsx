4e13005a3bd4a20c35f82d9383845c3d43a3daae
Miguel Chavez <toonchavez8@gmail.com>
2023-05-25 21:38:48 -0600

    add btns and icons

--- src/components/landingPage/Hero.jsx
+++ src/components/landingPage/Hero.jsx
@@ -1,6 +1,8 @@
+import "./Hero.css";
 import { useEffect, useState } from "react";
 import heroImg from "../../assets/landing/Hero_Banner.png";
 import PropTypes from "prop-types";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 export default function Hero(
 	{ heroTaglines } // heroTaglines is an array of strings
@@ -21,14 +23,25 @@ export default function Hero(
 
 	return (
 		<section className="relative">
-			<h1 className="absolute text-center z-10 text-white uppercase text-3xl ff-nunito w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-				{heroTagline}
-			</h1>
+			<div className="absolute text-center z-10 text-white uppercase  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+				<h1 className="ff-libre-baskerville text-5xl  ">{heroTagline}</h1>
+				<div className="ff-nunito ">
+					<button className="border-2 text-white uppercase px-4 py-2 rounded mt-4">
+						Conoce el estudio
+						<AiOutlineArrowRight className="inline-block ml-2" />
+					</button>
+
+					<button className="border-2 text-white uppercase px-4 py-2 rounded mt-4 ml-4">
+						Nuestro Trabajo
+						<AiOutlineArrowRight className="inline-block ml-2" />
+					</button>
+				</div>
+			</div>
 			<figure className="bg-black">
 				<img
 					src={heroImg}
 					alt="hero banner"
-					className="h-96 w-full object-cover opacity-75"
+					className="max-h-96 w-full object-cover opacity-50"
 				/>
 			</figure>
 		</section>
