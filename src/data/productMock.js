1d05757f8b18726adfe1d295f88d1d87bdce7d01
Miguel Chavez <toonchavez8@gmail.com>
2023-06-13 00:09:07 -0600

    create mock data

---
+++ src/data/productMock.js
@@ -0,0 +1,68 @@
+export const products = [
+	{
+		id: 1,
+		name: "Flashes Profoto D1 Air 500W",
+		price: 1200,
+		description: "Flashes Profoto D1 Air 500W con 2x Softbox Profoto 3x4",
+		category: "Flashes",
+		image:
+			"https://cdn.shopify.com/s/files/1/0565/8057/1308/products/ProfotoD1Air500DuoLightKit.jpg?v=1629503388&width=533",
+		available: true,
+		quantity: 3,
+	},
+	{
+		id: 2,
+		name: "Flashes Profoto D1 Air 1000W",
+		price: 1500,
+		description: "Flashes Profoto D1 Air 1000W con 2x Softbox Profoto 3x4",
+		category: "Flashes",
+		image:
+			"https://www.fotomecanica.mx/media/catalog/product/cache/243b585d5b053344651ac1ff3b7a4649/p/r/profoto_901025_d1_air_1000_w_s_1547571062_605742.jpg",
+		available: false,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		name: "Aputure c300d II",
+		price: 1000,
+		description: "Aputure c300d II con tripie",
+		category: "Illuminacion",
+		image:
+			"https://www.fotomecanica.mx/media/catalog/product/cache/243b585d5b053344651ac1ff3b7a4649/a/p/aputure_lsc300diivkit_light_storm_c300d_mark_1560940549_1483694.jpg",
+		available: true,
+		quantity: 3,
+	},
+	{
+		id: 4,
+		name: "Aputure c120d II",
+		price: 800,
+		description: "Aputure c120d II con tripie",
+		category: "Illuminacion",
+		image:
+			"https://m.media-amazon.com/images/I/41evjuAxgBL._AC_UF1000,1000_QL80_.jpg",
+		available: true,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		name: "Boom Manfrotto",
+		price: 300,
+		description: "Boom Manfrotto para carcar luces",
+		category: "accesorios",
+		image:
+			"https://http2.mlstatic.com/D_NQ_NP_882903-MLM31238363379_062019-O.webp",
+		available: true,
+		quantity: 1,
+	},
+	{
+		id: 6,
+		name: "Cicloramas de color",
+		price: 300,
+		description: "Cicloramas de color 2x2",
+		category: "accesorios",
+		image:
+			"https://scontent.fgdl13-1.fna.fbcdn.net/v/t39.30808-6/284093659_5110753619003495_4335713186427184220_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=GUOafBRuAEwAX_s3rTH&_nc_ht=scontent.fgdl13-1.fna&oh=00_AfBQ35MPAR8azF5o_1xWot87MtLwGZ9q_EJLILBdWXCsHQ&oe=648D193F",
+		available: true,
+		quantity: 8,
+	},
+];
