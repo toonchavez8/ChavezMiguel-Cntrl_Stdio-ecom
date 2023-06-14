782bccaf41a2db5e518a0cd794c69ae74e64ff9a
Miguel Chavez <toonchavez8@gmail.com>
2023-06-13 18:39:50 -0600

    create menu routesjs and product detail container

---
+++ src/components/landingPage/studioHighlights/StudioHighlights.jsx
@@ -0,0 +1,41 @@
+export default function StudioHighlights() {
+	return (
+		<section className="flex flex-col w-full debug">
+			<div className="flex flex-row items-center justify-center w-full  ">
+				<h2 className="">
+					El lugar ideal para tu producción de foto o video, con la mejor
+					ubicación en Guadalajara.
+				</h2>
+
+				<article className="">
+					<p>
+						Siempre trataremos de hacerte sentir como en casa, te proporcionamos
+						el espacio para que puedas crear y convertir tus ideas en imágenes
+						impactantes.
+					</p>
+					<p>Tu renta incluye</p>
+					<ul className="flex flex-wrap">
+						<li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2">
+							Ciclorama blanco y negro
+						</li>
+						<li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2">
+							Wifi simétrico de 200 MB
+						</li>
+						<li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2">
+							Primer carga de café de cortesía
+						</li>
+						<li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2">
+							Dispensador de agua fría y caliente
+						</li>
+						<li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2">
+							Frigobar
+						</li>
+						<li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2">
+							Altavoz Bluetooth Bose
+						</li>
+					</ul>
+				</article>
+			</div>
+		</section>
+	);
+}
---
+++ src/pages/productDetail/ProductDetailContainter.jsx
@@ -0,0 +1,5 @@
+import ProductDetail from "./ProductDetail";
+
+export default function ProductDetailContainter() {
+	return <ProductDetail />;
+}
