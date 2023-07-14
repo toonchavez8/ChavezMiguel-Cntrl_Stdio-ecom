4792511a58b5935697901a23abe66446ad206e32
Miguel Chavez <toonchavez8@gmail.com>
2023-07-14 00:09:21 -0600

    update checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,3 +1,79 @@
-export default function Checkout() {
-	return <div>Checkout</div>;
+import { useFormik } from "formik";
+import { InputField } from "../../components/common/Forms/InputField";
+import PropTypes from "prop-types";
+
+export default function Checkout({
+	onSubmit,
+	initialValues,
+	validationSchema,
+}) {
+	const { handleSubmit, handleChange, errors, values } = useFormik({
+		initialValues,
+		onSubmit,
+		validateOnChange: false,
+		validationSchema,
+	});
+
+	return (
+		<main className="w-full">
+			<section className="w-full flex flex-col mt-24 gap-5 justify-center items-center debug">
+				<h1 className="">meter orden</h1>
+
+				<ul className="steps steps-vertical md:steps-horizontal">
+					<li className="step step-primary">Nombre</li>
+					<li className="step step-primary">Correo</li>
+					<li className="step">Fecha</li>
+					<li className="step">Enviar solictud</li>
+				</ul>
+
+				<article className="flex flex-col items-center justify-center  card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-200">
+					<form
+						className="card-body form-control w-full"
+						onSubmit={handleSubmit}
+					>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text text-accent font-medium">
+									Nombre
+								</span>
+							</label>
+							<InputField
+								label="Nombre"
+								name="name"
+								placeholder="Nombre"
+								type="text"
+								handleChange={handleChange}
+								value={values.name}
+								error={errors.name}
+							/>
+							<label className="label">
+								<span className="label-text text-accent font-medium">
+									Correo
+								</span>
+							</label>
+							<InputField
+								label="Correo"
+								name="email"
+								placeholder="Correo"
+								type="text"
+								handleChange={handleChange}
+								value={values.name}
+								error={errors.name}
+							/>
+						</div>
+
+						<button className="btn btn-primary mt-5" type="submit">
+							Enviar
+						</button>
+					</form>
+				</article>
+			</section>
+		</main>
+	);
 }
+
+Checkout.propTypes = {
+	onSubmit: PropTypes.func.isRequired,
+	initialValues: PropTypes.object.isRequired,
+	validationSchema: PropTypes.object.isRequired,
+};
--- src/pages/Checkout/CheckoutContainer.jsx
+++ src/pages/Checkout/CheckoutContainer.jsx
@@ -1,5 +1,31 @@
 import Checkout from "./Checkout";
+import * as Yup from "yup";
 
 export default function CheckoutContainer() {
-	return <Checkout />;
+	const onSubmit = (data) => {
+		console.log(`mensaje de ${data.name} fue enviado`);
+	};
+
+	const initialValues = {
+		name: "",
+		email: "",
+		date: "",
+	};
+
+	const validationSchema = Yup.object({
+		name: Yup.string()
+			.required("Campo requerido")
+			.min(3, "Mínimo 3 caracteres"),
+		email: Yup.string().required("Campo requerido").email("Email inválido"),
+		message: Yup.string()
+			.required("Campo requerido")
+			.min(10, "Mínimo 10 caracteres"),
+	});
+	return (
+		<Checkout
+			onSubmit={onSubmit}
+			initialValues={initialValues}
+			validationSchema={validationSchema}
+		/>
+	);
 }
