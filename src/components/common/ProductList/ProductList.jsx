bead24869662bd123ec96a99b90be13011245a64
Miguel Chavez <toonchavez8@gmail.com>
2023-07-14 23:07:24 -0600

    update product list

--- src/components/common/ProductList/ProductList.jsx
+++ src/components/common/ProductList/ProductList.jsx
@@ -20,7 +20,7 @@ ProductList.propTypes = {
 
 export default function ProductList({ products }) {
 	return (
-		<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-12  	">
+		<section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-6 py-12 mx-12 sm:mx-24">
 			{products.map((product) => (
 				<ProductCard key={product.id} product={product} />
 			))}
