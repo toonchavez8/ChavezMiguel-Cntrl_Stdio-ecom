b3139498b7a57119ea6e5304c05c811c9f30ff20
Miguel Chavez <toonchavez8@gmail.com>
2023-12-13 14:20:19 -0600

    Feat: add react scroll to nav

--- src/components/landingPage/EspaciosDispnibles/EspaciosDisponibles.jsx
+++ src/components/landingPage/EspaciosDispnibles/EspaciosDisponibles.jsx
@@ -4,7 +4,10 @@ import EstudioImg from "../../../assets/landing/Estudio-1.png";
 
 const EspaciosDisponibles = () => {
 	return (
-		<section className="flex-col justify-start items-center gap-28 inline-flex my-20 w-full max-w-7xl">
+		<section
+			id="conoce-los-espacios"
+			className="flex-col justify-start items-center gap-28 inline-flex my-20 w-full max-w-7xl"
+		>
 			<h2 className="max-w-4xl text-xl px-2 w-84 text-stone-900 md:px-2 lg:px-2 md:w-[30rem] lg:w-full md:text-3xl lg:text-6xl font-semibold ff-barlow text-center ff-barlow mt-16 group transition-all duration-200 ease-in-out">
 				Conoce los{" "}
 				<span className="text-accent group-hover:text-primary group-hover:scale-105">
