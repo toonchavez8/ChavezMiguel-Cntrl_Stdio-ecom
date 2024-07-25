49d98cc1a4c568958007f0980230be475a90fda1
Miguel Chavez <toonchavez8@gmail.com>
2024-07-25 11:17:52 -0600

    refactor(app): Remove context and Menu Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout/Layout";
 
 import { menuRoutes } from "./routes/menuRoutes";
-import CartContextProvider from "./context/cartContext";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<CartContextProvider>
-				<Routes>
-					<Route element={<Layout />}>
-						{menuRoutes.map(({ id, path, Element }) => (
-							<Route key={id} path={path} element={<Element />} />
-						))}
-					</Route>
-				</Routes>
-			</CartContextProvider>
+			<Routes>
+				<Route element={<Layout />}>
+					{menuRoutes.map(({ id, path, Element }) => (
+						<Route key={id} path={path} element={<Element />} />
+					))}
+				</Route>
+			</Routes>
 		</BrowserRouter>
 	);
 }
